5517951d80751655ce9bd8716026dcc8232b8989
EarthW0rm <marco.a.faustino@gmail.com>
2018-08-25 01:38:57 -0300

    Configurando i18n na index.

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,30 @@
-import 'modules/bootstrap/dist/js/bootstrap'
-import 'modules/font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/js/bootstrap';
+import 'font-awesome/css/font-awesome.min.css';
+import './sass/main';
 
-import './sass/main'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { HashRouter } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
+import promise from 'redux-promise';
+// import multi from 'redux-multi';
+import thunk from 'redux-thunk';
 
-import promise from 'redux-promise'
-import multi from 'redux-multi'
-import thunk from 'redux-thunk'
+import App from './main/app';
+import reducers from './main/reducers';
 
-import App from './main/app'
-import reducers from './main/reducers'
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(thunk,  promise)(createStore)(reducers, devTools);
 ReactDOM.render(<Provider store={store}>
-                    <HashRouter>
-                        <App />
-                    </HashRouter>
-                </Provider>
-                , document.getElementById('app'));
+    <I18nextProvider i18n={i18n}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </I18nextProvider>
+</Provider>
+    , document.getElementById('app'));
