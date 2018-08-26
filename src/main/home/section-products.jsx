2d144cea890a6c017c01c5c4fc80a4bbb8e5824b
EarthW0rm <marco.a.faustino@gmail.com>
2018-08-26 00:35:25 -0300

    Section products

--- src/main/home/section-products.jsx
+++ src/main/home/section-products.jsx
@@ -6,43 +6,60 @@ export default translate()((props) => {
     const { t, tReady } = props;
     return (
         <section className="section-products" id="section-products">
-            <div className="row">
-                <div className="col">
+            <div className="row line-product">
+                <div className="col product">
                     <div>
+                        <div className="ico">
+                            <i className="fas fa-th"></i>
+                        </div>
                         <h2>{t('products.a.title')}</h2>
-                        <hr />
+                        <hr className="separator" />
                         <p>{t('products.a.p')}</p>
                     </div>
                 </div>
 
-                <div className="col">
+                <div className="col product dark">
                     <div>
+                        <div className="ico">
+                            <i className="fas fa-receipt"></i>
+                        </div>
                         <h2>{t('products.b.title')}</h2>
-                        <hr />
+                        <hr className="separator" />
                         <p>{t('products.b.p')}</p>
                     </div>
                 </div>
 
-                <div className="col">
+                <div className="col product">
                     <div>
+                        <div className="ico">
+                            <i className="fab fa-bitcoin"></i>
+                        </div>
                         <h2>{t('products.c.title')}</h2>
-                        <hr />
+                        <hr className="separator" />
                         <p>{t('products.c.p')}</p>
                     </div>
                 </div>
 
-                <div className="col">
+                <div className="col product dark">
                     <div>
-                        <h2>{t('products.d.title')}</h2>
-                        <hr />
+                        <div className="ico">
+                            <i className="far fa-comments"></i>
+                        </div>
+                        <div className="title">
+                            <h2>{t('products.d.title')}</h2>
+                        </div>
+                        <hr className="separator" />
                         <p>{t('products.d.p')}</p>
                     </div>
                 </div>
 
-                <div className="col">
+                <div className="col product">
                     <div>
+                        <div className="ico">
+                            <i className="fa fa-check-double"></i>
+                        </div>
                         <h2>{t('products.e.title')}</h2>
-                        <hr />
+                        <hr className="separator" />
                         <p>{t('products.e.p')}</p>
                     </div>
                 </div>
