975f6628eb036e3ceb3ca6be81031ea639181b0c
EarthW0rm <marco.a.faustino@gmail.com>
2018-08-26 04:50:30 -0300

    Adicionando imagens dos funcionarios.

--- src/main/home/section-team.jsx
+++ src/main/home/section-team.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { translate } from 'react-i18next';
 
+import ft1 from '../../images/_team-coo.jpg';
+import ft2 from '../../images/_team-ceo.jpg';
+import ft3 from '../../images/_team-cto.jpg';
+import ft4 from '../../images/_team-exp.png';
+
 export default translate()((props) => {
     const { t, tReady } = props;
     return (
@@ -14,21 +19,33 @@ export default translate()((props) => {
                         </div>
                         <div className="row">
                             <div className="col">
+                                <div className="photo">
+                                    <img src={ft1} />
+                                </div>
                                 <h3>{t('team.people.a.name')}</h3>
                                 <span>{t('team.people.a.cargo')}</span>
                                 <p>{t('team.people.a.press')}</p>
                             </div>
                             <div className="col">
+                                <div className="photo">
+                                    <img src={ft2} />
+                                </div>
                                 <h3>{t('team.people.b.name')}</h3>
                                 <span>{t('team.people.b.cargo')}</span>
                                 <p>{t('team.people.b.press')}</p>
                             </div>
                             <div className="col">
+                                <div className="photo">
+                                    <img src={ft3} />
+                                </div>
                                 <h3>{t('team.people.c.name')}</h3>
                                 <span>{t('team.people.c.cargo')}</span>
                                 <p>{t('team.people.c.press')}</p>
                             </div>
                             <div className="col">
+                                <div className="photo">
+                                    <img src={ft4} />
+                                </div>
                                 <h3>{t('team.people.d.name')}</h3>
                                 <span>{t('team.people.d.cargo')}</span>
                                 <p>{t('team.people.d.press')}</p>
